33a3ebf183d7dda13052defc4a42a362dff12088
samy <samysamyromioromio@gmail.com>
2025-04-16 17:09:35 +0200

    Preparing the work environment

---
+++ Medical_CenterAPI/Medical_CenterAPI/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Medical_CenterAPI.Models;
+
+namespace Medical_CenterAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+
+
+
+        } 
+    }
+}
---
+++ Medical_CenterAPI/Medical_CenterAPI/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Medical_CenterAPI.Models
+{
+    public class AppUser :IdentityUser
+    {
+    }
+}
