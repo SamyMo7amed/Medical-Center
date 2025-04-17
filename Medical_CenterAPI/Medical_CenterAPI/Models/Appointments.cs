79c1e54fa131bb30528467188ff0cf612930f090
salspil <salsabilami598@gmail.com>
2025-04-17 23:18:45 +0200

    new updat

---
+++ Medical_CenterAPI/Medical_CenterAPI/Models/Appointments.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Medical_CenterAPI.Models
+{
+    public enum Appointment_Status
+    {
+        Pending,
+        Confirmed,
+        Canceled
+    }
+    public class Appointment
+    {
+        [Key]
+        public Guid AppointmentId { get; set; }
+
+        
+        // 1-patiant
+
+        public Guid PatiantId { get; set; }
+        [ForeignKey(nameof(PatiantId))]
+        public Patiant Patiant { get; set; }
+
+        // 2-doctor
+
+        public Guid DoctorId { get; set; }
+        [ForeignKey(nameof(DoctorId))]
+        public Doctor Doctor { get; set; }
+
+        // 3-Assistant
+
+        public Guid AssistantId { get; set; }
+        [ForeignKey(nameof(AssistantId))]
+        public Assistant Assistant { get; set; }
+
+        // 4- data 
+
+        public DateTime AppointmentDate { get; set; }
+
+        // 5-status
+
+        public Appointment_Status Status { get; set; }
+
+        // 6-CreatedAt
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+    }
+}
