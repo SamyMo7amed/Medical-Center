24f39d54f1bf35fdbd6adf7e629151628c71a83e
samy <samysamyromioromio@gmail.com>
2025-04-20 21:56:28 +0200

    edit on programm.cs

--- Medical_CenterAPI/Medical_CenterAPI/Program.cs
+++ Medical_CenterAPI/Medical_CenterAPI/Program.cs
@@ -9,8 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
@@ -43,6 +42,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
