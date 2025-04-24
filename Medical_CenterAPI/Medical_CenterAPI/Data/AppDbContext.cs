06687d1db61e5c1df34eadb87d2bcbcae780b530
Samy Mohamed <116796859+SamyMo7amed@users.noreply.github.com>
2025-04-24 10:53:19 +0200

    Update AppDbContext.cs

--- Medical_CenterAPI/Medical_CenterAPI/Data/AppDbContext.cs
+++ Medical_CenterAPI/Medical_CenterAPI/Data/AppDbContext.cs
@@ -23,7 +23,7 @@ namespace Medical_CenterAPI.Data
         public DbSet<AppointmentConfirmation> AppointmentConfirmations { get; set; }
         public DbSet<Assistant> Assistants { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
-        public DbSet<Patiant> Patiants { get; set; }
+        public DbSet<Patient> Patients { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
